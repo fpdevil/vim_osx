9addaceaad2c96b7bdf63348cd0e6ec517c28c55
singamsetty <singamsetty.sampath@gmail.com>
2017-07-18 02:16:16 +0530

    updated ycm configuration location details

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,9 +1,7 @@
 import os
 import os.path
-import fnmatch
 import logging
 import ycm_core
-import re
 
 
 # `gcc -print-prog-name=cpp` -v
@@ -60,6 +58,22 @@ HEADER_EXTENSIONS = [
         '.hpp',
         '.hh'
 ]
+
+# Set this to the absolute path to the folder (NOT the file!) containing the
+# compile_commands.json file to use that instead of 'flags'. See here for
+# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
+#
+# Most projects will NOT need to set this to anything; you can just change the
+# 'flags' list of compilation flags.
+compilation_database_folder = ''
+
+if os.path.exists( compilation_database_folder ):
+  database = ycm_core.CompilationDatabase( compilation_database_folder )
+else:
+  database = None
+
+def DirectoryOfThisScript():
+  return os.path.dirname( os.path.abspath( __file__ ) )
 
 def IsHeaderFile(filename):
     extension = os.path.splitext(filename)[1]
